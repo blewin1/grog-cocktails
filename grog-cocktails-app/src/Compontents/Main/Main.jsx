2af265a4df3847adc22761ed1277a6fefce0ae0c
blewin1 <blewin1@gmail.com>
2020-07-02 11:08:16 -0400

    made random reload on reclick

--- grog-cocktails-app/src/Compontents/Main/Main.jsx
+++ grog-cocktails-app/src/Compontents/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from '../Home/Home'
 import SingleSearch from '../SingleSearch/SingleSearch'
@@ -19,7 +19,7 @@ const Main = () => {
                 <Route path="/byIngredients" component={IngredientsSearch} />
                 <Route path="/byGlass" render={() => <SingleSearch title='Search by Glass' type='glass' />} />
                 <Route path="/details/:id" component={DrinkByID} />
-                <Route path="/random" component={RandomDrink} />
+                <Route path="/random" render={() => <RandomDrink key={Math.random()} />} />
                 <Route path="*" render={() => <Redirect to='/' />} />
             </Switch>
         </main>
